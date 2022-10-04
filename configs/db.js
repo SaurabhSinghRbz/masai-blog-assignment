991329fa09bed570f2c943fd7ca6b65502a75bd1
Saurabh <singhsaurabh162000@gmail.com>
2022-10-04 11:53:18 +0530

    uploading all server data to deploy

---
+++ configs/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+let database = async () => {
+    mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+        .then(() => console.log('Connected to database'))
+        .catch(err => console.log(err));
+}
+
+module.exports = database;
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const database = require('./configs/db');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post.routes');
+const userRouter = require('./routes/user.routes');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    return res.send('Hello World!');
+});
+
+
+
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/blogs", postRouter);
+
+
+
+app.listen(8080, () => {
+    database();
+    console.log("Listening on port 8080");
+});
