79bc84d52f5ac936e2d9117a6e6a8d33b19daf0b
Saurabh <singhsaurabh162000@gmail.com>
2022-10-06 00:49:46 +0530

    update

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 let database = async () => {
     mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
